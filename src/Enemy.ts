bbceb24779c09e5a38773c977147606f70dc7d0c
Justin de Ruiter <djjustin13@gmail.com>
2018-05-16 17:11:42 +0200

    Reverted abel's wrong commit

--- src/Enemy.ts
+++ src/Enemy.ts
@@ -1,26 +1,73 @@
+<<<<<<< HEAD
+module MyGame{
+=======
 module myGame {
 
     export class Enemy extends Phaser.Sprite {
         
         constructor(game: Phaser.Game, x: number, y: number) {
+>>>>>>> e205f4cfe17d69bddb9d45e0230b394f95612d48
 
-            super(game, x, y, 'enemy', 0); // Name
+    export class Enemy extends Phaser.Sprite{
+
+        enemyState: number
+        timer: number
+        facing: number
+
+        constructor(game: Phaser.Game, x: number, y: number){
+            
+            super(game, x, y, 'dude', 0);
 
             this.game.physics.arcade.enableBody(this)
             this.body.collideWorldBounds=true;
 
             this.anchor.setTo(0.5, 0);
 
-            // this.animations.add('right', [5, 6, 7, 8], 10, true)
-            // this.animations.add('left', [5, 6, 7, 8], 10, true)
+            this.animations.add('right', [5, 6, 7, 8], 10, true)
+            this.animations.add('left', [5, 6, 7, 8], 10, true)
 
             game.add.existing(this);
+
+            this.enemyState = 0;
+            this.timer = 0;
+            this.facing = 1;
+            this.body.velocity.x = 100;
         }
 
+<<<<<<< HEAD
+        update(){
+            this.body.bounce.y = 0.0;
+            this.body.gravity.y = 300;
+            this.scale.x = this.facing;
+
+            switch(this.enemyState){
+                case 0: //idle
+                    this.timer++;
+                    if (this.timer > 100){
+                        this.timer = 0;
+                        if(this.facing == 1){
+                            this.facing = -1;
+                            this.body.velocity.x = -100;
+                            this.animations.stop();
+                            this.animations.play('left');
+                        }
+                        //if(this.facing == 0){
+                            else{
+                            this.facing = 1;
+                            this.body.velocity.x = 100;
+                            this.animations.stop();
+                            this.animations.play('right');
+                        }
+                    }
+
+                    break;
+            }
+=======
         update(){            
             this.body.velocity.x = 0;
             this.body.bounce.y = 0.2;
             this.body.gravity.y = 300;
+>>>>>>> e205f4cfe17d69bddb9d45e0230b394f95612d48
         }
     }
 }
