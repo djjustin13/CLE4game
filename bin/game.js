56a57b21566894f1ee9a7c5ae76bf6ad7cec416c
Justin de Ruiter <djjustin13@gmail.com>
2018-05-16 16:49:54 +0200

    Fixed merge conflict

--- bin/game.js
+++ bin/game.js
@@ -110,7 +110,7 @@ var MyGame;
             var h = this.game.world.height;
             this.game.world.setBounds(0, 0, 6000, 600);
             this.background = this.add.tileSprite(0, 0, this.world.width, 600, 'bgTile');
-            this.platforms = this.game.add.physicsGroup();
+            this.platforms = this.add.group();
             this.platforms.enableBody = true;
             this.ground = this.add.tileSprite(0, h - 32, this.world.width, 32, 'platformTile');
             this.game.physics.arcade.enableBody(this.ground);
@@ -121,26 +121,35 @@ var MyGame;
             this.ledge.body.immovable = true;
             this.ledge = this.platforms.create(0, 400, 'ground');
             this.ledge.body.immovable = true;
-            this.objects = this.add.group();
-            this.objects.create(600, h - 80, 'item');
+            this.artPiece = this.add.group();
+            this.artPiece.enableBody = true;
+            for (var i = 0; i < 4; i++) {
+                this.artPiece.create(250 + 100 * i, this.world.height - 90, 'artPiece');
+            }
+            this.game.physics.arcade.enable(this.artPiece);
             this.enemy = this.add.group();
             this.enemy.enableBody = true;
             this.enemy = this.enemy.create(260, 360, 'eye');
             this.game.physics.arcade.enable(this.enemy);
-            this.player = new MyGame.Player(this.game, 30, 284);
-            this.game.physics.arcade.enable(this.player);
+            this.player = new MyGame.Player(this.game, 130, 284);
             this.game.camera.follow(this.player);
         };
         Level1.prototype.update = function () {
+            var _this = this;
             this.physics.arcade.collide(this.player, this.platforms);
             this.physics.arcade.collide(this.player, this.ground);
             this.physics.arcade.collide(this.player, this.enemy);
+            this.physics.arcade.collide(this.platforms, this.artPiece);
+            this.physics.arcade.overlap(this.player, this.artPiece, function () { return _this.collectArtPiece(); });
             if (this.game.input.keyboard.isDown(Phaser.Keyboard.ESC)) {
                 this.resetLevel();
             }
         };
         Level1.prototype.resetLevel = function () {
             this.game.state.start('Level1', true, false);
+        };
+        Level1.prototype.collectArtPiece = function () {
+            this.artPiece.kill();
         };
         return Level1;
     }(Phaser.State));
--- src/Level1.ts
+++ src/Level1.ts
@@ -1,78 +1,8 @@
-<<<<<<< HEAD
 module MyGame {
 
 	export class Level1 extends Phaser.State {
 		background: Phaser.TileSprite
 		ground: Phaser.TileSprite
-		ledge: Phaser.Sprite
-		player: MyGame.Player
-		platforms: Phaser.Group
-		objects: Phaser.Group
-		enemy: Phaser.Group
-
-
-		create() {
-			let h = this.game.world.height
-			this.game.world.setBounds(0, 0, 6000, 600)
-			this.background = this.add.tileSprite(0, 0, this.world.width, 600, 'bgTile')
-
-			// Creation of platforms: ground, platforms, ledges e.d.
-			this.platforms = this.game.add.physicsGroup()
-			this.platforms.enableBody = true
-			
-			this.ground = this.add.tileSprite(0, h-32, this.world.width, 32, 'platformTile')
-			this.game.physics.arcade.enableBody(this.ground)
-			this.ground.body.collideWorldBounds = true;
-			this.ground.body.immovable = true;
-			this.ground.body.allowGravity = false;
-
-			this.ledge = this.platforms.create(400, h-120, 'ground')
-			this.ledge.body.immovable = true
-			this.ledge = this.platforms.create(0, 400, 'ground')
-			this.ledge.body.immovable = true
-
-			this.objects = this.add.group()
-			this.objects.create(600 ,h-80, 'item')
-			
-			// Creation of Enemies
-			this.enemy = this.add.group()
-			this.enemy.enableBody = true
-
-			this.enemy = this.enemy.create(260, 360, 'eye');
-			this.game.physics.arcade.enable(this.enemy);
-
-			// Creation of the player
-			this.player = new Player(this.game, 30, 284);
-			this.game.physics.arcade.enable(this.player);
-
-			this.game.camera.follow(this.player)
-			
-
-			
-		}
-
-		update(){
-
-			this.physics.arcade.collide(this.player, this.platforms)
-			this.physics.arcade.collide(this.player, this.ground)
-			this.physics.arcade.collide(this.player, this.enemy)
-
-			if (this.game.input.keyboard.isDown(Phaser.Keyboard.ESC)){
-				this.resetLevel()
-			}
-		}
-
-		resetLevel(){
-			this.game.state.start('Level1', true, false);
-		}
-	}
-
-=======
-module MyGame {
-
-	export class Level1 extends Phaser.State {
-		background: Phaser.TileSprite
-		ground: Phaser.Sprite
 		ledge: Phaser.Sprite
 		player: MyGame.Player
 		platforms: Phaser.Group
@@ -85,21 +15,20 @@ module MyGame {
 			this.game.world.setBounds(0, 0, 6000, 600);
 			this.background = this.add.tileSprite(0, 0, this.world.width, 600, 'bgTile')
 
-
 			// Creation of platforms: ground, platforms, ledges e.d.
 			this.platforms = this.add.group()
 			this.platforms.enableBody = true
 
-			this.ground = this.platforms.create(0, this.world.height - 32, 'ground')
-			this.ground.body.immovable = true
-			this.ground.width = this.world.height
-			this.ground.scale.x = 100
+			this.ground = this.add.tileSprite(0, h-32, this.world.width, 32, 'platformTile')
+			this.game.physics.arcade.enableBody(this.ground)
+			this.ground.body.collideWorldBounds = true;
+			this.ground.body.immovable = true;
+			this.ground.body.allowGravity = false;
 
-			this.ledge = this.platforms.create(400, 400, 'ground')
+			this.ledge = this.platforms.create(400, h-120, 'ground')
 			this.ledge.body.immovable = true
-			this.ledge = this.platforms.create(-150, 250, 'ground')
+			this.ledge = this.platforms.create(0, 400, 'ground')
 			this.ledge.body.immovable = true
-
 
 			// Creation of Puzzle pieces
 			this.artPiece = this.add.group()
@@ -120,21 +49,28 @@ module MyGame {
 			// Creation of the player
 			this.player = new Player(this.game, 130, 284);
 			this.game.camera.follow(this.player)
-			console.log("level started")
 		}
 
 		update(){
 			this.physics.arcade.collide(this.player, this.platforms)
-			this.physics.arcade.collide(this.player, this.platforms)
+			this.physics.arcade.collide(this.player, this.ground)
 			this.physics.arcade.collide(this.player, this.enemy)
 			this.physics.arcade.collide(this.platforms, this.artPiece)
-			this.physics.arcade.overlap(this.player, this.artPiece, collectArtPiece, null, this)
+			this.physics.arcade.overlap(this.player, this.artPiece, () => this.collectArtPiece())
 
-			function collectArtPiece(){
-				this.artPiece.kill();
+			if (this.game.input.keyboard.isDown(Phaser.Keyboard.ESC)){
+				this.resetLevel()
 			}
+
+		}
+		
+		resetLevel(){
+			this.game.state.start('Level1', true, false);
+		}
+
+		collectArtPiece(){
+			this.artPiece.kill();
 		}
 	}
 
->>>>>>> e205f4cfe17d69bddb9d45e0230b394f95612d48
 } 
