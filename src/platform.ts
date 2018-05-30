06d42c55251e3b9f6b5d112e402889a905d2c38c
0882916 <0882916@hr.nl>
2018-05-30 09:56:21 +0200

    platform name changes

---
+++ src/platform.ts
@@ -0,0 +1,22 @@
+module MyGame {
+
+    export class Platform extends Phaser.Sprite {
+
+        constructor(game: Phaser.Game, x: number, y: number) {
+
+            super(game, x, y, 'platform', 0);
+
+            this.game.physics.arcade.enableBody(this)
+            this.body.collideWorldBounds=true;
+
+            this.anchor.setTo(0.5, 0.5);
+
+            game.add.existing(this);
+        }
+
+        update() {
+            this.body.bounce.y = 0.0;
+            this.body.gravity.y = 0.0;
+        }
+    }
+}
