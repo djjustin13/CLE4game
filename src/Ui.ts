711c62ea34a9869227a0b1e58a33caa7871f8112
Justin de Ruiter <djjustin13@gmail.com>
2018-06-11 12:08:26 +0200

    Added pause function

--- src/Ui.ts
+++ src/Ui.ts
@@ -2,17 +2,44 @@ module MyGame {
 
     export class Ui{
         game: Game
+        pauseButton:Phaser.Sprite
+
         constructor(g:Game){
             this.game = g
             let ui:Phaser.Sprite = this.game.add.sprite(this.game.width, 0, 'uiBase');
             ui.anchor.setTo(1, 0)
             ui.fixedToCamera = true
 
-            let pauseButton:Phaser.Sprite = this.game.add.sprite(0, 0, 'gamePause');
-            pauseButton.fixedToCamera = true
+            this.pauseButton = this.game.add.sprite(0, 0, 'gamePause');
+            this.pauseButton.fixedToCamera = true
+
+            this.pauseButton.inputEnabled = true
+			this.pauseButton.input.useHandCursor = true;
+
+			this.pauseButton.events.onInputDown.add(() => this.pauseLevel())
+			this.pauseButton.events.onInputOver.add(() => this.hover(this.pauseButton))
+            this.pauseButton.events.onInputOut.add(() => this.hoverOut(this.pauseButton))
+            
+            this.game.input.onDown.add(() => this.unPause(), self)
 
         }
 
-    }
+        hover(el:Phaser.Sprite){
+			el.scale.setTo(1.05, 1.05)
+		}
 
+		hoverOut(el:Phaser.Sprite){
+			el.scale.setTo(1, 1)
+        }
+
+        pauseLevel(){
+            this.game.paused = true
+            console.log("Pauze")
+        }
+
+        unPause(){
+            console.log("cliick")
+            if(this.game.paused == true)this.game.paused = false
+        }
+    }
 }
