6238a6bc24b4e4f94645e4850d575c9bd9ba7e62
abel <abel-1993@hotmail.com>
2018-06-11 12:05:40 +0200

    Added longlegs sprites

--- src/Spikes.ts
+++ src/Spikes.ts
@@ -6,6 +6,8 @@ module MyGame{
             super(game, x, y, 'spikes', 0);
 
             this.game.physics.arcade.enableBody(this)
+            
+            this.body.immovable = true;
 
             game.add.existing(this);
         }
